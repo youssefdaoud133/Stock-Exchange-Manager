463f2c07eb98e5338246b6b7c91b80f854af3c3b
MaiMahmoud182 <maielfeky2004@gmail.com>
2024-05-01 15:02:53 +0300

    sign up scene

--- src/main/java/oop/stockexchangemanager/HelloController.java
+++ src/main/java/oop/stockexchangemanager/HelloController.java
@@ -19,19 +19,9 @@ public class HelloController  {
     private TextField Username;
     @FXML
     private PasswordField Password;
-    @FXML
-    private DatePicker birthdate;
-    @FXML
-    private ChoiceBox <String> choice;
-    private final String[] gender = {"Male", "Female"};
-    @FXML
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        choice.getItems().addAll(gender);
-        choice.setOnAction(this::getGender);
-    }
-    public void getGender(ActionEvent event){
-        String myGender = choice.getValue();
-    }
+
+
+
     public void  login() throws IOException {
         Username.getText();
         Password.getText();
@@ -42,6 +32,7 @@ public class HelloController  {
         primaryStage.setTitle("StockExchangeManager");
         primaryStage.show();
     }
+    @FXML
     public void  newAccount() throws IOException {
         Stage primarystage=(Stage) newAccount.getScene().getWindow();
         Stage primaryStage=new Stage();
@@ -50,9 +41,6 @@ public class HelloController  {
         primaryStage.setTitle("StockExchangeManager");
         primaryStage.show();
     }
-    public void getDate(ActionEvent event){
-        LocalDate myDate = birthdate.getValue();
-        String dateFormat = myDate.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
-        birthdate.getValue();
-    }
+
+
 }
---
+++ src/main/java/oop/stockexchangemanager/SignupController.java
@@ -0,0 +1,48 @@
+package oop.stockexchangemanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class SignupController {
+
+    public DatePicker birthdate;
+    public ChoiceBox choiceBox;
+    public Label Test;
+
+    @FXML
+    public void initialize(){
+        choiceBox.getItems().addAll("Male", "Female");
+//        choice.setOnAction(this::getGender);
+
+    };
+
+    public void getDate(ActionEvent actionEvent) {
+    }
+
+
+    public void getGender() {
+        // Get the selected item from the choiceBox
+        Object selectedItem = choiceBox.getValue();
+
+        // Check if the selected item is not null and is an instance of String
+        if (selectedItem != null && selectedItem instanceof String) {
+            // Cast the selected item to String
+            String selectedGender = (String) selectedItem;
+
+            // Update your label with the selected gender
+            Test.setText("Selected Gender: " + selectedGender);
+        } else {
+            // Handle the case where no item is selected or the selected item is not a String
+            Test.setText("Please select a gender");
+        }
+    }
+
+
+}
