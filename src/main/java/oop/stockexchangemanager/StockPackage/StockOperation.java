c95fff3b154650be9e03636fcb8b2b82e2b67649
YoussefDaoud <youssefdaoud0122@gmail.com>
2024-05-05 16:17:55 +0300

    add branch

--- src/main/java/oop/stockexchangemanager/StockPackage/StockOperation.java
+++ src/main/java/oop/stockexchangemanager/StockPackage/StockOperation.java
@@ -4,7 +4,7 @@ package oop.stockexchangemanager.StockPackage;
 import oop.stockexchangemanager.Database.Stocks;
 
 public class StockOperation {
-    public static Stock AddStock(String companyName, float price, int Quantity, String AdminName,int AdminId){
+    public static Stock AddStock( String companyName, float price, int Quantity, String AdminName,int AdminId){
         Stock stock = Stock.Generate(companyName,price,Quantity,AdminName,AdminId);
         return  Stocks.getInstance().create(stock.getId(),stock);
     }
