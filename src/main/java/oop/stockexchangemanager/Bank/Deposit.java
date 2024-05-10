ed1e9d2722e91203c4a5a155827646e4c385b249
MaiMahmoud182 <maielfeky2004@gmail.com>
2024-05-10 04:42:20 +0300

    edit deposit

--- src/main/java/oop/stockexchangemanager/Bank/Deposit.java
+++ src/main/java/oop/stockexchangemanager/Bank/Deposit.java
@@ -1,2 +1,21 @@
+package oop.stockexchangemanager.Bank;
 
+public class Deposit implements BankOperation {
+    private static Deposit instance;
+
+    // Private constructor to prevent instantiation outside this class
+    private Deposit() {}
+
+    // Method to get the instance of Deposit
+    public static Deposit getInstance() {
+        if (instance == null) {
+            instance = new Deposit();
+        }
+        return instance;
+    }
+    @Override
+    public void DoOperation(BankAccount account, float amount) {
+        account.setBalance(account.getBalance() + amount);
+    }
+}
 
