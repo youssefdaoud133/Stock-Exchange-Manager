8c9669e098b0a3ad5850ff957153b704335fc6f5
Salmaa-Ali <salma.ali04@eng-st.cu.edu.eg>
2024-05-15 19:26:37 +0300

    Sell card added

---
+++ src/main/java/oop/stockexchangemanager/SellCardController.java
@@ -0,0 +1,20 @@
+package oop.stockexchangemanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SellCardController {
+    @FXML
+    private Label companyName;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private Button sellButton;
+
+    public void sellOperation(){
+    }
+}
